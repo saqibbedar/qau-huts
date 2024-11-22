6c01062e69c38f8e8a29c4ed335e50f8656993fc
saqibbedar <chandio.saqib.m@gmail.com>
2024-11-22 17:46:25 +0500

    feat: add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,25 @@
+import { IoBag, IoSearch } from "react-icons/io5";
+import { MdAccountCircle } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <>
-      
+      <header className="flex items-center justify-between bg-sky-300">
+        <Link to={"/"} className="logo">QHUTS</Link>
+        <ul className="flex gap-4">
+          <li><Link to={"/home"}>Home</Link></li>
+          <li><Link to={"/about"}>About</Link></li>
+          <li className="cursor-pointer">Huts</li>
+          <li><Link to={"/contact"}>Contact</Link></li>
+        </ul>
+
+        <ul className="flex gap-2">
+          <li><Link to={"/orders"}><IoBag/></Link></li>
+          <li><Link to={"/search"}><IoSearch /></Link></li>
+          <li><Link to={"/profile"}><MdAccountCircle/></Link></li>
+        </ul>
+      </header>
     </>
   );
 };
